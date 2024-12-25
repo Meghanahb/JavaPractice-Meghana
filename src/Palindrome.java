b59d727fde6211f3605e961b0b2b483f60a962f4
Meghana <meghanabasavanna@gmail.com>
2024-12-25 16:15:25 +0530

    StringPalindromeOrNot

---
+++ src/Palindrome.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Palindrome
+{
+    public static  void main(String[] args)
+    {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter the String:");
+        String str= obj.next();
+        int len=str.length();
+        String orgstr=str;
+        String reversStr="";
+        //char[] array=str.toCharArray();
+        for(int i=len-1;i>=0;i--)
+        {
+            reversStr=reversStr+str.charAt(i);
+        }
+        //if(orgstr.equalsIgnoreCase(orgstr.equals(reversStr)))
+         if(orgstr.equals(reversStr))
+            System.out.println("The given String is Palindrome");
+            else
+                System.out.println("The given string is Not Palindrome");
+    }
+}
